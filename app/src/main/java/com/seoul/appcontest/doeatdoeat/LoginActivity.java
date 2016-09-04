c441d477e3cf04586ea3d613d232ffe61ae8bb0d
KeonHee <kimgh6554@hanmail.net>
2016-09-04 19:04:57 +0900

    init commit

---
+++ app/src/main/java/com/seoul/appcontest/doeatdoeat/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.seoul.appcontest.doeatdoeat;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+/**
+ * Created by user on 2016-09-02.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String Tag = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+    @InjectView(R.id.input_email) EditText _emailText;
+    @InjectView(R.id.input_password) EditText _passwordText;
+    @InjectView(R.id.btn_login) Button _loginButton;
+    @InjectView(R.id.link_signup) TextView _signupLink;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.inject(this);
+    }
+}
---
+++ app/src/main/java/com/seoul/appcontest/doeatdoeat/MainActivity.java
@@ -0,0 +1,32 @@
+package com.seoul.appcontest.doeatdoeat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String Tag = "MainActivity";
+
+    @InjectView(R.id.go_login) Button _gologinpageButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        _gologinpageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "로그인 페이지로 이동합니다", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
