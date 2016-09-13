f3afbe9325fc08d9b1a90ca1dbf2dee9491cde24
KeonHee <kimgh6554@hanmail.net>
2016-09-14 02:31:00 +0900

    ADD - add facebook login

--- app/src/main/java/com/seoul/appcontest/doeatdoeat/LoginActivity.java
+++ app/src/main/java/com/seoul/appcontest/doeatdoeat/LoginActivity.java
@@ -20,17 +20,26 @@ import android.widget.Toast;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -42,12 +51,14 @@ public class LoginActivity extends FragmentActivity {
 
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
-    private CallbackManager mCallbackManager;
-    private AccessToken mToken = null;
+    private CallbackManager callbackManager;
+    private AccessToken token = null;
     private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authListener;
     @InjectView(R.id.email) AppCompatEditText _emailText;
     @InjectView(R.id.password) AppCompatEditText _passwordText;
     @InjectView(R.id.btnLogin) Button _loginButton;
+    @InjectView(R.id.btnFacebookLogin) Button _loginFacebookButton;
     @InjectView(R.id.btnForgotPassword) Button _forgotpasswordLink;
     @InjectView(R.id.btnLinkToRegisterScreen) TextView _signupLink;
 
@@ -72,8 +83,66 @@ public class LoginActivity extends FragmentActivity {
          */
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(getApplication());;
-        mCallbackManager = CallbackManager.Factory.create();
-        mToken = AccessToken.getCurrentAccessToken();
+        callbackManager = CallbackManager.Factory.create();
+        token = AccessToken.getCurrentAccessToken();
+
+        _loginFacebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this,
+                        Arrays.asList("public_profile", "user_friends","email"));
+            }
+        });
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(final LoginResult result) {
+                GraphRequest request;
+                request = GraphRequest.newMeRequest(result.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject user, GraphResponse response) {
+                        if (response.getError() != null) {
+                            Log.i("TAG", "GraphResponse Error" );
+                        } else {
+                            final String token= result.getAccessToken().getToken();
+                            Log.i("TAG", "user: " + user.toString());
+                            Log.i("TAG", "AccessToken: " + token);
+                            auth.signInWithCredential(FacebookAuthProvider.getCredential(token))
+                                    .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            if(task.isSuccessful()){
+                                                Toast.makeText(LoginActivity.this, "로그인 성공!", Toast.LENGTH_SHORT).show();
+                                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                                startActivity(intent);
+                                                finish();
+                                            }else{
+                                                Toast.makeText(LoginActivity.this, "로그인 실패ㅜㅜ", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+
+                            setResult(RESULT_OK);
+                            //finish();
+                        }
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+            @Override
+            public void onError(FacebookException error) {
+                Log.e("test", "Error: " + error);
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+                finish();
+            }
+        });
+
 
 
 
@@ -93,7 +162,6 @@ public class LoginActivity extends FragmentActivity {
                     return;
                 }
 
-                //authenticate user_icon_64
                 auth.signInWithEmailAndPassword(email,password)
                         .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
@@ -135,6 +203,12 @@ public class LoginActivity extends FragmentActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
 }
