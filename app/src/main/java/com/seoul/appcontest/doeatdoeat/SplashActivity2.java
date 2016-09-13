f80cfd5e886d641328866f1dd79ec12ec4be9efe
KeonHee <kimgh6554@hanmail.net>
2016-09-13 22:43:49 +0900

    UPDATE - change main ui

--- app/src/main/java/com/seoul/appcontest/doeatdoeat/SplashActivity2.java
+++ app/src/main/java/com/seoul/appcontest/doeatdoeat/SplashActivity2.java
@@ -8,19 +8,19 @@ import android.os.Handler;
 /**
  * Created by user_icon_64 on 2016-09-11.
  */
-public class SplashActivity extends Activity{
+public class SplashActivity2 extends Activity{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash2);
+        setContentView(R.layout.activity_splash);
         Handler hd = new Handler();
         hd.postDelayed(new splashhandler() , 2000); // 5초 후에 hd Handler 실행
     }
 
     private class splashhandler implements Runnable{
         public void run() {
-            startActivity(new Intent(getApplication(), SplashActivity2.class)); // 로딩이 끝난후 이동할 Activity
-            SplashActivity.this.finish(); // 로딩페이지 Activity Stack에서 제거
+            startActivity(new Intent(getApplication(), LoginActivity.class)); // 로딩이 끝난후 이동할 Activity
+            SplashActivity2.this.finish(); // 로딩페이지 Activity Stack에서 제거
             overridePendingTransition(R.anim.fade,R.anim.cycle_7);
         }
     }
